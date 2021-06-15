fc00203036214df899aea2f636089de589a1af82
zivuch <zivuch@gmail.com>
2021-06-16 01:11:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
 const app = express();
-// const server = http.createServer(app);
-const io = socketio(app);
+const server = http.createServer(app);
+const io = socketio(server);
 
 io.on('connection', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
@@ -53,4 +53,4 @@ app.get('/artem', (req,res)=>{
   res.send('hello artem')
 })
 
-app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
